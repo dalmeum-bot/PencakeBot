0a633acecfc0a2cd09bf7c5e995dac439b52803a
Rhseung <rhseungg@gmail.com>
2022-12-29 09:14:45 +0900

    BOJ 연동

--- bot.py
+++ bot.py
@@ -1,19 +1,166 @@
 import discord
-import json
-from discord.ext import commands
+import random
+from pathlib import Path
+from urllib.request import urlopen
+from urllib.request import Request
+from urllib.error import HTTPError
+from json import loads
+from bs4 import BeautifulSoup
 
-bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+# make
+class Pencake(discord.Bot):
+	async def on_ready(self):
+		print(f'Logged in as {self.user}')
 
-@bot.event
-async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print('------')
-    await bot.change_presence(status=discord.Status.online, activity=None)
+	async def on_member_join(self, member):
+		role = discord.utils.get(member.guild.roles, name="User")
+		member.add_roles(role)
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+bot = Pencake()
+config = loads(Path('config.json').read_text())
 
-bot.run(json.load(open('config.json', 'r'))['token'])
+# BOJ commands
+BOJ = discord.SlashCommandGroup(name='boj', description='BOJ commands', guild_ids=config['guild_ids'].values())
+tier_names = ['Unrated', 'Bronze', 'Silver', 'Gold', 'Platinum', 'Diamond', 'Ruby']
+tier_counts = ['V', 'IV', 'III', 'II', 'I']
+tier_colors = [0x302C2C, 0xA85C1C, 0x4C5C7C, 0xE49C34, 0x6CDCAC, 0x54B4F4, 0xEC3464]
+
+try:
+	@BOJ.command(name='id', description='search BOJ problem')
+	async def problem_search(ctx, problem_id: int):
+		url = f'https://solved.ac/api/v3/problem/show?problemId={problem_id}'
+
+		try:
+			objects = loads(BeautifulSoup(urlopen(url).read(), 'html.parser').get_text())
+		except HTTPError:
+			await ctx.respond(f'**{problem_id}**번 문제를 찾을 수 없습니다.')
+			return
+		except UnicodeError:
+			await ctx.respond('아이디는 유니코드가 들어가지 않습니다.')
+			return
+		except:
+			await ctx.respond('알 수 없는 오류가 발생했습니다.')
+			return
+
+		problem = {
+			"id": objects['problemId'],
+			"title": objects['titleKo'],
+			"tier": tier_names[0] if objects['level'] == 0
+			else tier_names[(objects['level'] - 1) // 5 + 1] + ' ' + tier_counts[(objects['level'] - 1) % 5],
+			"tier_icon": f"https://static.solved.ac/tier_small/{objects['level']}.svg",
+			"tier_color": tier_colors[0] if objects['level'] == 0
+			else tier_colors[(objects['level'] - 1) // 5 + 1],
+			"tags": [x['displayNames'][0]['name'] for x in objects['tags']]
+		}
+
+		embed = discord.Embed(
+			title=f"{problem['id']}: {problem['title']}",
+			description=problem['tier'],
+			url=f"https://www.acmicpc.net/problem/{problem['id']}",
+			color=problem['tier_color']
+		)
+
+		if problem['tags']:
+			embed.add_field(name="Tags", value='\n'.join(problem['tags']))
+
+		class Button(discord.ui.View):
+			@discord.ui.button(label=f"{problem['id']}번 펼치기", style=discord.ButtonStyle.primary)
+			async def info(self, button, interaction):
+				info = discord.Embed(
+					title=f"{problem['id']}: {problem['title']}",
+					description=problem['tier'],
+					url=f"https://www.acmicpc.net/problem/{problem['id']}",
+					color=problem['tier_color']
+				)
+
+				req = Request(f"https://www.acmicpc.net/problem/{objects['problemId']}",
+				              headers={'User-Agent': 'Mozila/5.0'})
+				webpage = urlopen(req)
+				soup = BeautifulSoup(webpage, features="html.parser")
+
+				problem['info'] = {
+					"problem_description": soup.find('div', id='problem_description').text.strip(),
+					"problem_input": soup.find('div', id='problem_input').text.strip(),
+					"problem_output": soup.find('div', id='problem_output').text.strip(),
+					"sample_inputs": [],
+					"sample_outputs": []
+				}
+
+				c = 1
+				while soup.find('pre', id=f'sample-output-{c}') is not None:
+					problem['info']['sample_inputs'].append(soup.find('pre', id=f'sample-input-{c}').text)
+					problem['info']['sample_outputs'].append(soup.find('pre', id=f'sample-output-{c}').text)
+					c += 1
+
+				info.add_field(name="Problem", value=f"```\n{problem['info']['problem_description']}\n```", inline=False)
+				info.add_field(name="Input", value=f"```\n{problem['info']['problem_input']}\n```", inline=False)
+				info.add_field(name="Output", value=f"```\n{problem['info']['problem_output']}\n```", inline=False)
+
+				for i in range(len(problem['info']['sample_inputs'])):
+					info.add_field(name=f"Sample Input {i+1}", value=f"```\n{problem['info']['sample_inputs'][i]}\n```", inline=False)
+					info.add_field(name=f"Sample Output {i+1}", value=f"```\n{problem['info']['sample_outputs'][i]}\n```", inline=False)
+
+				if problem['tags']:
+					info.add_field(name="Tags", value='\n'.join(problem['tags']), inline=False)
+
+				await interaction.response.edit_message(embed=info, view=self)
+
+			@discord.ui.button(label=f"{problem['id']}번 접기", style=discord.ButtonStyle.primary)
+			async def info_2(self, button, interaction):
+				await interaction.response.edit_message(embed=embed, view=self)
+
+		await ctx.respond(embed=embed, view=Button())
+
+	@BOJ.command(name='user', description='search BOJ user')
+	async def user_search(ctx, user_name: str):
+		url = f'https://solved.ac/api/v3/search/user?query={user_name}'
+
+		try:
+			objects = loads(BeautifulSoup(urlopen(url).read(), 'html.parser').get_text())['items'][0]
+		except IndexError:
+			await ctx.respond(f'**{user_name}**라는 아이디를 가진 유저를 찾을 수 없습니다.')
+			return
+		except UnicodeError:
+			await ctx.respond('아이디는 유니코드가 들어가지 않습니다.')
+			return
+		except:
+			await ctx.respond('알 수 없는 오류가 발생했습니다.')
+			return
+
+		user = {
+			"name": objects['handle'],
+			"bio": objects['bio'],
+			"organizations": [x['name'] for x in objects['organizations']],
+			"profile_image": objects['profileImageUrl'],
+			"solved_count": objects['solvedCount'],
+			"rating": objects['rating'],
+			"tier": 'Master' if objects['tier'] == 31
+			else tier_names[(objects['tier'] - 1) // 5 + 1] + ' ' + tier_counts[(objects['tier'] - 1) % 5],
+			"tier_color": random.choice([0xd989c1, 0xa0e4fc, 0xa6c0fb, 0xe685b5, 0xaca0fb, 0xb494f4]) if objects[
+				                                                                                             'tier'] == 31
+			else tier_colors[(objects['tier'] - 1) // 5 + 1],
+			"rank": objects['rank']
+		}
+
+		embed = discord.Embed(
+			title=user['name'],
+			description=user['bio'],
+			url=f"https://solved.ac/profile/{user['name']}",
+			color=user['tier_color']
+		)
+
+		embed.add_field(name="Tier", value=user['tier'])
+		embed.add_field(name="Rating", value=user['rating'])
+		embed.add_field(name="Rank", value=f"#{user['rank']}")
+
+		if user['profile_image']:
+			embed.set_thumbnail(url=user['profile_image'])
+		if user['organizations']:
+			embed.add_field(name="Organization", value='\n'.join(user['organizations']), inline=False)
+
+		await ctx.respond(embed=embed)
+except Exception as error:
+	print(error)
+
+bot.add_application_command(BOJ)
+bot.run(config['token'])
